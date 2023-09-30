e5d29b4c9779c66b9e65a65dc7ce186bcf3057a5
ben <ben.worsley@matillion.com>
2023-09-30 21:57:49 +0100

    feat: first go at Works section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -40,10 +40,10 @@ const ExperienceCard = ({ experience }) => (
 const Experience = () => {
   return (
     <>
-      <motion variants={textVariant()} >
+      <motion.div variants={textVariant()} >
         <p className={styles.sectionSubText}>What I have done so far</p>
         <h2 className={styles.sectionHeadText}>Work Experience.</h2>
-      </motion>
+      </motion.div>
       <div className="mt-20 flex flex-col">
         <VerticalTimeline>
           {experiences.map((experience, index) => (
